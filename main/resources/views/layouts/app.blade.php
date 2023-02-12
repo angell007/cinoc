df2864c1f66942cadba44df03836096e7e75b997
angell007 <mdgrisalez@misena.edu.co>
2023-02-12 18:47:26 -0500

    add ico

--- main/resources/views/layouts/app.blade.php
+++ main/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 
 // if (!isset($seo)) {
 
-    $seo = (object)array('seo_title' => $siteSetting->site_name, 'seo_description' => $siteSetting->site_name, 'seo_keywords' => $siteSetting->site_name, 'seo_other' => '');
+$seo = (object) ['seo_title' => $siteSetting->site_name, 'seo_description' => $siteSetting->site_name, 'seo_keywords' => $siteSetting->site_name, 'seo_other' => ''];
 
 // }
 
@@ -10,7 +10,8 @@
 
 <!DOCTYPE html>
 
-<html lang="{{ app()->getLocale() }}" class="{{ (session('localeDir', 'ltr'))}}" dir="{{ (session('localeDir', 'ltr'))}}">
+<html lang="{{ app()->getLocale() }}" class="{{ session('localeDir', 'ltr') }}"
+    dir="{{ session('localeDir', 'ltr') }}">
 
 
 
@@ -22,7 +23,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{__($seo->seo_title) }}</title>
+    <title>{{ __($seo->seo_title) }}</title>
 
     <meta name="Description" content="{!! $seo->seo_description !!}">
 
@@ -30,55 +31,54 @@
 
     <meta name="csrf-token" id="csrf_token" content="{{ csrf_token() }}">
 
-
-
     {!! $seo->seo_other !!}
 
     <!-- Fav Icon -->
 
-    <link rel="shortcut icon" href="http://etitc.edu.co/uploads/images/products/5f343aad0575e873393158.svg">
+    <link rel="shortcut icon" href="https://bolsaempleo.iescinoc.edu.co/images/logo.jpeg">
 
     <!-- Slider -->
 
-    <link href="{{asset('/')}}js/revolution-slider/css/settings.css" rel="stylesheet">
+    <link href="{{ asset('/') }}js/revolution-slider/css/settings.css" rel="stylesheet">
 
     <!-- Owl carousel -->
 
-    <link href="{{asset('/')}}css/owl.carousel.css" rel="stylesheet">
+    <link href="{{ asset('/') }}css/owl.carousel.css" rel="stylesheet">
 
     <!-- Bootstrap -->
 
-    <link href="{{asset('/')}}css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('/') }}css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome -->
 
-    <link href="{{asset('/')}}css/font-awesome.css" rel="stylesheet">
+    <link href="{{ asset('/') }}css/font-awesome.css" rel="stylesheet">
 
     <!-- Custom Style -->
 
-    <link href="{{asset('/')}}css/main.css?r=6" rel="stylesheet">
+    <link href="{{ asset('/') }}css/main.css?r=6" rel="stylesheet">
 
-    @if((session('localeDir', 'ltr') == 'rtl'))
+    @if (session('localeDir', 'ltr') == 'rtl')
+        <!-- Rtl Style -->
 
-    <!-- Rtl Style -->
-
-    <link href="{{asset('/')}}css/rtl-style.css" rel="stylesheet">
-
+        <link href="{{ asset('/') }}css/rtl-style.css" rel="stylesheet">
     @endif
 
-    <link href="{{ asset('/') }}admin_assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/') }}admin_assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css"
+        rel="stylesheet" type="text/css" />
 
-    <link href="{{ asset('/') }}admin_assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/') }}admin_assets/global/plugins/select2/css/select2.min.css" rel="stylesheet"
+        type="text/css" />
 
-    <link href="{{ asset('/') }}admin_assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('/') }}admin_assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet"
+        type="text/css" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 
     <!--[if lt IE 9]>
 
-          <script src="{{asset('/')}}js/html5shiv.min.js"></script>
+          <script src="{{ asset('/') }}js/html5shiv.min.js"></script>
 
-          <script src="{{asset('/')}}js/respond.min.js"></script>
+          <script src="{{ asset('/') }}js/respond.min.js"></script>
 
         <![endif]-->
 
@@ -87,50 +87,46 @@
 
 
     <style>
+        .custom-select {
 
-    
+            style="width: 150px;"
+            /*width: 100%;*/
 
-                .custom-select {
+            /*text-overflow: ellipsis;*/
 
-                    style="width: 150px;"
+            /*overflow: hidden;*/
 
-                    /*width: 100%;*/
+            /*max-width: 100%;*/
 
-                    /*text-overflow: ellipsis;*/
+            /*white-space:nowrap;*/
 
-                    /*overflow: hidden;*/
+        }
 
-                    /*max-width: 100%;*/
 
-                    /*white-space:nowrap;*/
 
-                }
+        option {
 
-                
 
-                option {
 
-                    
+            width: 200px !important;
 
-                    width: 200px !important;
+            /*text-overflow: ellipsis;*/
 
-                    /*text-overflow: ellipsis;*/
+            overflow: hidden;
 
-                    overflow: hidden;
+            /*max-width: 100%;*/
 
-                    /*max-width: 100%;*/
+            /*white-space:nowrap;*/
 
-                    /*white-space:nowrap;*/
 
-                    
 
-                 /*overflow: hidden;*/
+            /*overflow: hidden;*/
 
-                 white-space: normal;
+            white-space: normal;
 
-                }
+        }
 
-               
+
 
         @media only screen and (max-width: 600px) {
 
@@ -161,28 +157,27 @@
 
 
         }
-
     </style>
 
-    
+
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-159331368-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159331368-1"></script>
 
-<script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-  window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
 
-  function gtag(){dataLayer.push(arguments);}
-
-  gtag('js', new Date());
+        gtag('js', new Date());
 
 
 
-  gtag('config', 'UA-159331368-1');
-
-</script>
+        gtag('config', 'UA-159331368-1');
+    </script>
 
 
 
@@ -196,31 +191,36 @@
 
     <!-- Bootstrap's JavaScript -->
 
-    <script src="{{asset('/')}}js/jquery.min.js"></script>
+    <script src="{{ asset('/') }}js/jquery.min.js"></script>
 
-    <script src="{{asset('/')}}js/bootstrap.min.js"></script>
+    <script src="{{ asset('/') }}js/bootstrap.min.js"></script>
 
-    <script src="{{asset('/')}}js/popper.js"></script>
+    <script src="{{ asset('/') }}js/popper.js"></script>
 
     <!-- Owl carousel -->
 
-    <script src="{{asset('/')}}js/owl.carousel.js"></script>
+    <script src="{{ asset('/') }}js/owl.carousel.js"></script>
 
-    <script src="{{ asset('/') }}admin_assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+    <script src="{{ asset('/') }}admin_assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"
+        type="text/javascript"></script>
 
-    <script src="{{ asset('/') }}admin_assets/global/plugins/Bootstrap-3-Typeahead/bootstrap3-typeahead.min.js" type="text/javascript"></script>
+    <script src="{{ asset('/') }}admin_assets/global/plugins/Bootstrap-3-Typeahead/bootstrap3-typeahead.min.js"
+        type="text/javascript"></script>
 
     <!-- END PAGE LEVEL PLUGINS -->
 
-    <script src="{{ asset('/') }}admin_assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
+    <script src="{{ asset('/') }}admin_assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript">
+    </script>
 
     <script src="{{ asset('/') }}admin_assets/global/plugins/jquery.scrollTo.min.js" type="text/javascript"></script>
 
     <!-- Revolution Slider -->
 
-    <script type="text/javascript" src="{{ asset('/') }}js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}js/revolution-slider/js/jquery.themepunch.tools.min.js">
+    </script>
 
-    <script type="text/javascript" src="{{ asset('/') }}js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/') }}js/revolution-slider/js/jquery.themepunch.revolution.min.js">
+    </script>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
@@ -236,161 +236,161 @@
 
     <!-- Custom js -->
 
-    <script src="{{asset('/')}}js/script.js"></script>
+    <script src="{{ asset('/') }}js/script.js"></script>
 
     <script type="text/JavaScript">
 
         $(document).ready(function(){
 
-              $('.textC').on('input',function(e){
+                  $('.textC').on('input',function(e){
 
-         var words= $(this).val().split(" ").length+$(this).val().split("\n").length-1
+             var words= $(this).val().split(" ").length+$(this).val().split("\n").length-1
 
-         console.log(words,$(this).val(),e.target.innerHTML)
+             console.log(words,$(this).val(),e.target.innerHTML)
 
-     if(words>=250){
+         if(words>=250){
 
-        $(this).val(text)
+            $(this).val(text)
 
-     }else{ 
+         }else{ 
 
-         text=$(this).val()
+             text=$(this).val()
 
-     }
+         }
 
-      })
+          })
 
-            $("#description").on('keyup',function(){
+                $("#description").on('keyup',function(){
 
-                
+                    
 
-                console.log("initialized");
+                    console.log("initialized");
 
-            })
-
-            
-
-            $(".number-1").each(function() {$(this).val(currency($(this).val())); });
-
-            $(".number-1").on({
-
-    "focus": function (event) {
-
-        $(event.target).select();
-
-    },
-
-    /*"keyup": function (event) {
-
-        $(event.target).val(function (index, value ) {
-
-            return value.replace(/\D/g, "")
-
-                        .replace(/([0-9])([0-9]{2})$/, '$1.$2')
-
-                        .replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",");
-
-        });
-
-    }*/
-
-    "input":function (event) {
-
-         $(event.target).val(currency($(event.target).val()))
-
-    }
-
-    
-
-});            
-
-            $('select').select2();
-
-
-
-            $('.selected-remote').select2({
-
-            width:'resolve',
-
-            theme:'classic',
-
-            ajax: {
-
-                url: '/api/proffesions',
-
-                dataType: 'json'
-
-                }
-
-            });
-
-
-
-
-
-            $(document).scrollTo('.has-error', 2000);
-
-            });
-
-            function currency(textCurrency){
-
-              
-
-         var salary_from = textCurrency
-
-                 
-
-                console.log(salary_from,salary_from.includes('.00'))
-
-                salary_from=salary_from.replace('$','').replace(/,/g,'')
+                })
 
                 
 
-                if(salary_from.includes('.00')){
+                $(".number-1").each(function() {$(this).val(currency($(this).val())); });
 
-                   salary_from=salary_from.replace(".00","")
+                $(".number-1").on({
+
+        "focus": function (event) {
+
+            $(event.target).select();
+
+        },
+
+        /*"keyup": function (event) {
+
+            $(event.target).val(function (index, value ) {
+
+                return value.replace(/\D/g, "")
+
+                            .replace(/([0-9])([0-9]{2})$/, '$1.$2')
+
+                            .replace(/\B(?=(\d{3})+(?!\d)\.?)/g, ",");
+
+            });
+
+        }*/
+
+        "input":function (event) {
+
+             $(event.target).val(currency($(event.target).val()))
+
+        }
+
+        
+
+    });            
+
+                $('select').select2();
+
+
+
+                $('.selected-remote').select2({
+
+                width:'resolve',
+
+                theme:'classic',
+
+                ajax: {
+
+                    url: '/api/proffesions',
+
+                    dataType: 'json'
+
+                    }
+
+                });
+
+
+
+
+
+                $(document).scrollTo('.has-error', 2000);
+
+                });
+
+                function currency(textCurrency){
+
+                  
+
+             var salary_from = textCurrency
+
+                     
+
+                    console.log(salary_from,salary_from.includes('.00'))
+
+                    salary_from=salary_from.replace('$','').replace(/,/g,'')
+
+                    
+
+                    if(salary_from.includes('.00')){
+
+                       salary_from=salary_from.replace(".00","")
+
+                    }
+
+                    if(salary_from.includes('.0') && !salary_from.includes('.00')){
+
+                        console.log("paso")
+
+                      salary_from= salary_from.replace(".0","") 
+
+                      salary_from= salary_from.substring(0, salary_from.length - 1);
+
+                    }
+
+                
+
+                    console.log()
+
+                    if(salary_from!='' && !parseFloat(salary_from).toString().includes(NaN)){
+
+                    salary_from='$' + parseFloat(parseInt(salary_from), 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();  
+
+                    }else{
+
+                       salary_from='';
+
+                    }
+
+            return salary_from;
+
+        }
+
+                
+
+                function showProcessingForm(btn_id){		
+
+                $("#"+btn_id).val( 'Processing .....' );
+
+                $("#"+btn_id).attr('disabled','disabled');		
 
                 }
 
-                if(salary_from.includes('.0') && !salary_from.includes('.00')){
-
-                    console.log("paso")
-
-                  salary_from= salary_from.replace(".0","") 
-
-                  salary_from= salary_from.substring(0, salary_from.length - 1);
-
-                }
-
-            
-
-                console.log()
-
-                if(salary_from!='' && !parseFloat(salary_from).toString().includes(NaN)){
-
-                salary_from='$' + parseFloat(parseInt(salary_from), 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();  
-
-                }else{
-
-                   salary_from='';
-
-                }
-
-        return salary_from;
-
-    }
-
-            
-
-            function showProcessingForm(btn_id){		
-
-            $("#"+btn_id).val( 'Processing .....' );
-
-            $("#"+btn_id).attr('disabled','disabled');		
-
-            }
-
-        </script>
+            </script>
 
 </body>
 
